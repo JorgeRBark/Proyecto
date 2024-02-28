76d1b0da19462162d0b1a3d0761e8d7be0870d51
JorgeRBark <160195907+JorgeRBark@users.noreply.github.com>
2024-02-28 00:43:40 -0300

    Delete Proyecto_Final_Python/Proyecto directory

--- Proyecto_Final_Python/Proyecto/AppCoder/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from AppCoder import views
-
-urlpatterns = [
-    path('', views.inicio),
-    path('/estudiantes', views.estudiantes),
-    path('/ejercicios', views.ejercicios),
-]
